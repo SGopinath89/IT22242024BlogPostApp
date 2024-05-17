f1335d78496092514156523d4f471e4dfceaa42e
Bingusala <139640476+Bingusala@users.noreply.github.com>
2024-05-17 08:46:31 +0530

    Update App.jsx
    Update App.jsx

--- Blog Post Web Application/blogging website - frontend/src/App.jsx
+++ Blog Post Web Application/blogging website - frontend/src/App.jsx
@@ -1,17 +1,32 @@
 import Navbar from "./components/navbar.component";
 import { Routes, Route } from "react-router-dom";
-import UserAuthForm from  "./pages/userAuthForm.page"
+import UserAuthForm from "./pages/userAuthForm.page";
+import { createContext, useEffect, useState } from "react";
+import { lookInSession } from "./common/session";
+
+// Create a context for the user authentication state
+const UserContext = createContext({});
+
 const App = () => {
-    
+    const [userAuth, setUserAuth] = useState({ access_token: null });
+
+    // useEffect to check if a user is stored in the session on component mount
+    useEffect(() => {
+        const userInSession = lookInSession("user");
+        if (userInSession) {
+            setUserAuth(JSON.parse(userInSession));
+        }
+    }, []);
+
     return (
-        <Routes>
-            <Route path="/" element={<Navbar />}>
-                <Route path="signin" element={<UserAuthForm type="sign-in"/>} />
-                <Route path="signup" element={<UserAuthForm type="sign-up"/>} />
-            </Route>
-        </Routes>
-        
-    )
+        <UserContext.Provider value={{ userAuth, setUserAuth }}>
+            <Navbar /> {/* Ensure Navbar is outside of Routes so it's always displayed */}
+            <Routes>
+                <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+                <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+            </Routes>
+        </UserContext.Provider>
+    );
 }
 
 export default App;
