15291bc2d90689f8d9094db9222cfc1be94e3023
Apsara2001 <apsaraduanji2001@gmail.com>
2024-05-17 08:40:31 +0530

    Updated server.js

--- Blog Post Web Application/server/server.js
+++ Blog Post Web Application/server/server.js
@@ -1,14 +1,113 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import 'dotenv/config';
+import bcrypt from 'bcrypt';
+import { nanoid } from 'nanoid'; 
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
+
+//schema below
+import User from './Schema/User.js';
 const server = express();
+
 let PORT = 3000;
 
-mongoose.connect()
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+
+server.use(express.json());
+server.use(cors())
+mongoose.connect(process.env.DB_LOCATION, {
+    autoIndex: true
+})
+
+const formatDatatoSend = (user) => {
+
+    const access_token = jwt.sign({id: user._id},process.env.SECRET_ACCESS_KEY)
+    return {
+        access_token,
+        profile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname,
+    }
+}
+
+const generateUsername = async(email) => {
+    let username = email.split("@")[0];
+
+    let isUsernameNotUnique = await User.exists({"personal_info.username": username}).then((result) => result)
+    isUsernameNotUnique ? (username += nanoid().substring(0, 5) ): "";
+    return username;
+}
+
+server.post("/signup",(req, res)=>{
+    let {fullname,email,password}=req.body;
+
+    if(fullname.length <3){
+        return res.status(403).json({"error":"Fullname must be at least 3 letters long"})
+    }
+
+    if(!email.length){
+        return res.status(403).json({"error":"Enter email"})
+    }
+
+    if(!emailRegex.test(email)){
+        return res.status(403).json({"error":"Email is invalid"})
+    }
+
+    if(!passwordRegex.test(password)){
+        return res.status(403).json({"error":"Password should be 6 to 20 characters long with a nummeric, 1 lowercase and 1 uppercase"})
+    }
+
+    bcrypt.hash(password,10,async(err, hashed_password)=>{
+        let username = await generateUsername(email);
+
+        let user = new User({
+            personal_info: {fullname , email , password,username}
+        })
+
+        user.save().then((u) => {
+            return res.status(200).json(formatDatatoSend(u))
+        })
+
+        .catch(err => {
+
+            if(err.code == 11000){
+                return res.status(500).json({"error":"Email already exsits"})
+            }
+            return res.status(500).json({"err":err.message})
+        })
+    })
+})
+
+server.post("/signin", (req,res)=>{
+    let { email,password } = req.body;
+
+    User.findOne({ "personal_info.email": email }).then((user) =>{
+        if(!user){
+            return res.status(403).json({ "error":"Email not found" });
+        }
+        
+        bcrypt.compare(password, user.personal_info.password, (err,result) =>{
+            if(err){
+                return res.status(403).json({"error":"Error occured while login please try again"});
+            }
+            if(!result){
+                return res.status(403).json({"error":"Incorrect password"});
+            }else{
+                return res.status(403).json(formatDatatoSend(user))
+            }            
+        })
+        
+    })
+
+    .catch(err => {
+        console.log(err.message);
+        return res.status(500).json({ "error":err.message })
+    })
+})
 server.listen(PORT, ()=> {
     console.log('listening on port ' + PORT)
 })
 
-//username - manukamayurajith2001
-//password- CSZY6k5uSx4LWpVH
-//DB LOCATION-
- // mongodb+srv://manukamayurajith2001:<password>@blooging-web.nm2zilt.mongodb.net/?retryWrites=true&w=majority
