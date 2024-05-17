bd3d668098e99d866c9d5c1fa4ddc84224990a2d
Bingusala <139640476+Bingusala@users.noreply.github.com>
2024-05-17 08:48:31 +0530

    Update session.jsx
    Update session.jsx

---
+++ Blog Post Web Application/blogging website - frontend/src/common/session.jsx
@@ -0,0 +1,16 @@
+const storeInSession = (key,value) =>{
+    return  sessionStorage.setItem(key,value)
+}
+
+const lookInSession = (key) =>{
+    return  sessionStorage.getItem(key)
+}
+const removeFromSession = (key) =>{
+    return  sessionStorage.removeItem(key)
+}
+
+const logOutUser = () =>{
+    sessionStorage.clear();
+}
+
+export {storeInSession,lookInSession,removeFromSession,logOutUser}
