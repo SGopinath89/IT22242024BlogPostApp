1e435ffa026c3950ace260c2bfd3b2084be7ea8d
Disitha Ranasinghe <120271466+Disitha395ra@users.noreply.github.com>
2024-05-17 08:26:27 +0530

    Updating user authentication page
    Updating user authentication page

--- Blog Post Web Application/blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ Blog Post Web Application/blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,12 +1,76 @@
+import { useContext, useRef } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
 import { Link } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+import { storeInSession } from "../common/session";
+import {UserContext} from "../App"
+
 const userAuthForm = ({ type }) =>{
+    
+    const authForm = useRef();
+    let {userAuth: {acess_token},setUserAuth}=useContext(UserContext)
+    
+    console.log(acess_token);
+     const userAuthThroughServer = (serverRoute,formData)=>{
+        
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute,formData)
+        .then(({ data }) =>{
+            storeInSession("user",JSON.stringify(data))
+            setUserAuth(data)
+        })
+        .catch(({response}) => {
+            toast.error(response.data.error)
+        })
+        
+    }
+
+    
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+
+        let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+        
+        let form = new FormData(authForm.current);
+        let formData = {};
+
+        for(let [key,value] of form.entries()){
+            formData[key] = value;
+        }
+        let { fullname, email, password} = formData;
+
+
+        if(fullname){
+            if(fullname.length <3){
+                return toast.error("Fullname must be at least 3 letters long")
+            }
+        }
+       
+    
+        if(!email.length){
+            return toast.error("Enter email")
+        }
+    
+        if(!emailRegex.test(email)){
+            return toast.error("Email is invalid")
+        }
+    
+        if(!passwordRegex.test(password)){
+            return toast.error("Password should be 6 to 20 characters long with a nummeric, 1 lowercase and 1 uppercase")
+        }
+
+        userAuthThroughServer(serverRoute,formData);
+    }
     return( 
         <AnimationWrapper keyValue={type}>
             <section className="h-cover flex items-center justify-center">
-            <form className="w-[80%] max-w-[400px]">
+            <Toaster />
+            <form  className="w-[80%] max-w-[400px]">
                 <h1 className="text-4xl font-gelasion capitalize text-center
                 mb-24">
                 {type =="sign-in" ? "Welcome back":"Join us today"}
@@ -41,6 +105,7 @@ const userAuthForm = ({ type }) =>{
                     <button
                         className="btn-dark center mt-14"
                         type ="submit"
+                        onClick={handleSubmit}
                     >
                         {type.replace("-"," ")}
                     </button>
